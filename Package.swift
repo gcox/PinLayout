09e2a630a0d5285073a07851aadd6dba219d8ef3
George Cox <george@tundaware.com>
2021-02-16 05:51:01 -0500

    Adds dynamic library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "PinLayout",
     products: [
-        .library(name: "PinLayout", targets: ["PinLayout"])
+        .library(name: "PinLayout", targets: ["PinLayout"]),
+        .library(name: "PinLayoutDynamic", type: .dynamic, targets: ["PinLayout"])
     ],
     targets: [
         .target(name: "PinLayout", path: "Sources")
